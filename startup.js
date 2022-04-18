f56f8845d9c81afa0e41c70cf8bebd4ae6a151a0
suttonbm <suttonbm@gmail.com>
2022-04-18 07:01:00 -0500

    Added blank files for new CCs, placeholders for gang and bladeburner.

--- startup.js
+++ startup.js
@@ -1,36 +1,61 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	ns.run("buyserver.js", 1);
-	ns.run("hacknet.script", 1);
-	//ns.run("HODL.js", 1);
+	// Initialize:
+	
+	// Copy treehack to n00dles
+	ns.scp("treehack.js", "n00dles");
+	// Gain root on n00dles
+	ns.nuke("n00dles");
+	// Start treehack on n00dles
+	ns.exec("treehack.js", "n00dles");
+	// Copy hacknet to foodnstuff
+	ns.scp("hacknet.js", "foodnstuff");
+	// Run hacknet on foodnstuff
+	ns.exec("hacknet.js", "foodnstuff");
+	// Copy buyserver to foodnstuff
+	ns.scp("buyserver.js", "foodnstuff");
+	// Run buyserver on foodnstuff
+	ns.exec("buyserver.js", "foodnstuff");
+	// If we have formulas, run batch on n00dles
+	// Otherwise, run batch_start_specified on n00dles
+	await runHack(ns, "n00dles");
+
+	// Loop continuously every 30 minutes:
 	while (true) {
-		let seed = Math.floor(Math.random() * 100000000);
-		ns.run("treehack.js", 1);
-		if (ns.getHackingLevel() < 20) {
-			ns.run("basichack.script", 400, "n00dles");
-		} else if (ns.getHackingLevel() < 100) {
-			ns.run("basichack.script", 400, "joesguns");
-		} else if (ns.getHackingLevel() < 200) {
-			if (ns.fileExists("Formulas.exe", "home")) {
-				ns.run("batch.js", 1);
-			} else {
-				ns.run("batch_start.js", 1, seed);
-			}
-		} else if (ns.getHackingLevel() < 500) {
-			if (ns.fileExists("Formulas.exe", "home")) {
-				ns.scriptKill("batch_start.js", "home");
-				ns.run("batch.js", 1);
-			} else {
-				ns.run("batch_start.js", 1, seed);
-			}
-		} else {
-			if (ns.fileExists("Formulas.exe", "home")) {
-				ns.scriptKill("batch_start.js", "home");
-				ns.run("batch.js", 1, seed);
-			} else {
-				ns.run("batch_start.js", 1, seed);
+		await ns.sleep(30*60*1000);
+		
+		// If there is sufficient memory, run contractor.
+		if (!ns.isRunning("contractor.js", "home")) {
+			let freeRam = ns.getServerMaxRam("home") - ns.getServerUsedRam("home");
+			let contractorRam = ns.getScriptRam("contractor.js");
+			if (freeRam < contractorRam) {
+				ns.exec("contractor.js", "home");
 			}
 		}
-		await ns.sleep(600000);
+		
+		// If there is sufficient memory, run stock script.
+		if (!ns.isRunning("HODL.js", "home")) {
+			let freeRam = ns.getServerMaxRam("home") - ns.getServerUsedRam("home");
+			let stockRam = ns.getScriptRam("HODL.js");
+			if (freeRam < stockRam) {
+				ns.exec("HODL.js", "home");
+			}
+		}
+
+		// TODO: Figure out how to deal with corporation.
+
+		// Kill current batch and restart to ensure optimal target selected
+		await runHack(ns);
+	}
+	
+}
+
+async function runHack(ns, target="") {
+	ns.scriptKill("batch.js", "home");
+	ns.scriptKill("batch_start.js", "home");
+	if (ns.fileExists("Formulas.exe", "home")) {
+		ns.exec("batch.js", "home", 1, target);
+	} else {
+		ns.exec("batch_start.js", "home", 1, target);
 	}
 }
